da48ad77458c12f8af49f047b9105090ded0473b
Evgen65 <uevgeny1965@gmail.com>
2022-12-16 12:38:06 +0200

    Update ApplicationManager.java

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -2,25 +2,41 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     WebDriver wd;
     HelperStudentForm studentForm;
-    public void init(){
-        wd=new ChromeDriver();
 
+    public void init() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("load-extension=C:/Tools/3.15.2_0");
+        wd = new ChromeDriver(options);
 
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wd.navigate().to("https://demoqa.com/");
-        studentForm=new HelperStudentForm(wd);
+        studentForm = new HelperStudentForm(wd);
+        //Thread.sleep();
+        remturnToDome();
+
+    }
+
+    private void remturnToDome() {
+        List<String> tabs=new ArrayList<>(wd.getWindowHandles());
+        wd.switchTo().window(tabs.get(1)).close();
+        wd.switchTo().window(tabs.get(0));
+
 
     }
 
     public void stop() {
     }
+
     public HelperStudentForm studentForm() {
         return studentForm;
     }
